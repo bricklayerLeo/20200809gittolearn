e54fd420b195805e78f6e4e4a7dcefab0b8b8fa6
bricklayerLeo <873634131@qq.com>
2020-09-06 22:38:15 +0800

    啊阿达

--- gittolearn/src/components/HelloWorld.vue
+++ gittolearn/src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <h2>Essential Links</h2>
-
+    <h2>Essential Links</h2>dd
     <h2>Essential Links</h2>
   </div>
 </template>
